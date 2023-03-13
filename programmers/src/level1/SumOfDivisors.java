3c859ab74015ab18c9c57c6c4c43a8087202fd50
hyeeun <bettertmrw95@gmail.com>
2023-03-13 15:03:50 +0900

    약수의 합

--- programmers/src/level1/SumOfDivisors.java
+++ programmers/src/level1/SumOfDivisors.java
@@ -2,14 +2,25 @@ package level1;
 
 public class SumOfDivisors {
 
-// 문제 제목		
-		public static void main(String[] args) {
+// 약수의 합		
+	public static void main(String[] args) {
 		
-			System.out.println();
-		}
-		
-		// 1
-	
-
+		System.out.println(solution(123));
+		System.out.println(solution(987));
 	}
+	
+	
+	// 1
+	public static int solution(int n) {
+        int answer = 0;
+        String str = Integer.toString(n);
+        int [] intArr = new int[str.length()];
+        for ( int i=0; i<str.length(); i++) {
+            intArr[i] = str.charAt(i) - '0';
+            answer += intArr[i];
+        }
+        return answer;
+    }
+
+}
 
