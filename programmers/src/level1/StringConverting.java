a03d1fe27069564cf17952942646aed85def9974
hyeeun <bettertmrw95@gmail.com>
2023-03-16 23:32:35 +0900

    문자열을 정수로 바꾸기

---
+++ programmers/src/level1/StringConverting.java
@@ -0,0 +1,20 @@
+package level1;
+
+public class StringConverting {
+
+// 문자열을 정수로 바꾸기		
+		public static void main(String[] args) {
+		
+			System.out.println("1234");
+			System.out.println("-1234");
+		}
+		
+		
+		//
+		 public static int solution(String s) {
+		        int answer = Integer.parseInt(s);
+		        return answer;
+		    }
+
+	}
+
